6ae1cc819fb3ed0bb00da28acd5afbccfd589705
Usuario <Usuario@CPD02-M09>
2024-03-26 11:28:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppTabuada/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Digite a quantidade de multiplicações: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Tabuada do:" + numero);
+            for (int i = 1; i <= quantidade; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} X {i} = {resultado}");
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
